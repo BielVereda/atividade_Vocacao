7ffdfba87401a9e3921e4b14cd3d7d2bffec69f8
Gabriel dos Santos Vereda <165197600+BielVereda@users.noreply.github.com>
2024-08-21 15:41:29 -0300

    Update and rename passo2 to passo2.py

--- passo2.py
+++ passo2.py
@@ -1,19 +1,27 @@
 calc = int(input(""))
-x = float(input(""))
-y = float(input(""))
+x = 0
+y = 0
 
 if (calc == 1):
+    x = float(input(""))
+    y = float(input(""))
     resp1 = x + y
     print(resp1)
 
 if (calc == 2):
+    x = float(input(""))
+    y = float(input(""))
     resp2 = x - y
     print(resp2)
 
 if (calc == 3):
+    x = float(input(""))
+    y = float(input(""))
     resp3 = x * y
     print(resp3)
 
 if (calc == 4):
+    x = float(input(""))
+    y = float(input(""))
     resp4 = x / y
     print(resp4)
